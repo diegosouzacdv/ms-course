0270bfdbb77351b86cf49670016ea310d56351bb
Diego Souza <diegoguitaibanez@gmail.com>
2021-10-23 08:21:40 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/diego/hrpayroll/resources/PaymentResources.java
@@ -0,0 +1,29 @@
+package com.diego.hrpayroll.resources;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.diego.hrpayroll.entities.Payment;
+import com.diego.hrpayroll.services.PaymentService;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResources {
+	
+	@Autowired
+	private PaymentService service;
+	
+	@GetMapping("/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+		Payment payment = service.getPayment(workerId, days);
+		return Objects.nonNull(payment) ? ResponseEntity.ok(payment) : ResponseEntity.notFound().build();
+	}
+	
+	
+}
