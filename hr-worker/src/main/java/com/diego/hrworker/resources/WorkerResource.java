6d676c067aa2cbda0d35f255d9e4dcb18db30c44
Diego Souza <diegoguitaibanez@gmail.com>
2021-10-23 14:58:14 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/diego/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/diego/hrworker/resources/WorkerResource.java
@@ -3,7 +3,10 @@ package com.diego.hrworker.resources;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import com.diego.hrworker.services.WorkerService;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private WorkerService service;
@@ -28,6 +36,9 @@ public class WorkerResource {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Optional<Worker> worker = service.findById(id);
 		return worker.isPresent() ? ResponseEntity.ok(worker.get()) : ResponseEntity.notFound().build();
 	}
