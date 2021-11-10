3d6b9ea10cfd712ad857680e4c556bc416b66667
Diego Souza <diegoguitaibanez@gmail.com>
2021-11-10 11:01:23 -0300

    Ajuste projeto

--- hr-worker/src/main/java/com/diego/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diego.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.diego.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
