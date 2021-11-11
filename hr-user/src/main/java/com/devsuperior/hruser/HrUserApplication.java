e4a0b33c2358942b43d88a847bb87183b49b0e4d
Diego Souza <diegoguitaibanez@gmail.com>
2021-11-11 18:30:23 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -20,7 +20,7 @@ public class HrUserApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.err.println("BCRYPT = " + bCryptPasswordEncoder.encode("123456"));
+		System.out.println("BCRYPT = " + bCryptPasswordEncoder.encode("123456"));
 	}
 
 }
